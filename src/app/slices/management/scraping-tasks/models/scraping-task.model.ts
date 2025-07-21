f9a9ecda114b861b45fd653011fe28f0d86b10d2
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-21 11:27:24 +0300

    Add Scraping Task Model

---
+++ src/app/slices/management/scraping-tasks/models/scraping-task.model.ts
@@ -0,0 +1,15 @@
+import { ScrapingDomain } from "../../scraping-domains/models/scraping-domains.model";
+
+export interface ScrapingTask {
+  id: string;
+  publishedAt: Date;
+  completedAt?: Date;
+  scraperId?: string;
+  domainId: string;
+  scrapingExecutor?: {
+    scraperName: string;
+    hostname: string;
+    ipAddress: string;
+  };
+  domain?: ScrapingDomain
+}
