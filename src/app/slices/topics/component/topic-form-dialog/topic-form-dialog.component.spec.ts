09a31a44409ab2ac911211bc569557a13f076c25
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-08-06 20:54:34 +0300

    Ad Topic Form Dialog

---
+++ src/app/slices/topics/component/topic-form-dialog/topic-form-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopicFormDialogComponent } from './topic-form-dialog.component';
+
+describe('TopicFormDialogComponent', () => {
+  let component: TopicFormDialogComponent;
+  let fixture: ComponentFixture<TopicFormDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TopicFormDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TopicFormDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/slices/topics/topics.module.ts
+++ src/app/slices/topics/topics.module.ts
@@ -4,18 +4,27 @@ import { AllTopicsComponent } from './component/all-topics/all-topics.component'
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { TopicsRoutingModule } from './topics-routing.module';
+import { TopicFormDialogComponent } from './component/topic-form-dialog/topic-form-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DialogService } from 'primeng/dynamicdialog';
 
 
 
 @NgModule({
   declarations: [
-    AllTopicsComponent
+    AllTopicsComponent,
+    TopicFormDialogComponent
+  ],
+  providers:[
+    DialogService
   ],
   imports: [   
     CommonModule,
     TopicsRoutingModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TopicsModule { }
